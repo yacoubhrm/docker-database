f8bb1289dba537d1f1f24d846bde37e8af23f1e9
MaxenceEy <72600147+MaxenceEy@users.noreply.github.com>
2024-06-25 16:49:44 +0200

    Add files via upload

--- generate_data_clients.py
+++ generate_data_clients.py
@@ -1,5 +1,6 @@
 import psycopg2
 from faker import Faker
+from bcrypt import hashpw, gensalt  # Module bcrypt pour le hachage sécurisé
 import uuid  # Module pour générer des UUID uniques
 
 fake = Faker()
@@ -30,11 +31,17 @@ def generate_clients(n):
             fake.address(),
             fake.email(),
             fake.phone_number()[:20],  # Tronquer le numéro de téléphone à 20 caractères
-            fake.user_name(),
-            str(uuid.uuid4())[:12]  # Générer un mot de passe unique (UUID tronqué à 12 caractères)
+            fake.user_name(),  # Générer un nom d'utilisateur aléatoire
+            generate_hashed_password()  # Générer un mot de passe haché avec BCrypt
         )
         clients.append(client)
     return clients
+
+def generate_hashed_password():
+    raw_password = str(uuid.uuid4())[:12]  # Générer un mot de passe aléatoire
+    salt = gensalt()  # Générer un sel aléatoire pour BCrypt
+    hashed_password = hashpw(raw_password.encode('utf-8'), salt).decode('utf-8')
+    return hashed_password
 
 def insert_clients_data(conn, clients):
     cur = conn.cursor()
@@ -60,7 +67,7 @@ def main():
         )
 
         create_clients_table(conn)  # Créez la table clients si elle n'existe pas
-        clients = generate_clients(100)  # Générer 100 clients factices avec username et mot de passe uniques
+        clients = generate_clients(100)  # Générer 100 clients factices avec username et mot de passe hachés
         insert_clients_data(conn, clients)  # Insérer les données générées
         print("Data inserted successfully.")
     
